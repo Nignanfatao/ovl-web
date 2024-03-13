c8dd52196adc239cb7e9f8442d1b7787ced17ee5
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-03-13 15:27:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
@@ -9,15 +9,15 @@ require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
-})
+});
 app.use('/qr', qr);
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/main.html')
-})
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:` + PORT)
-})
+});
 
 module.exports = app
