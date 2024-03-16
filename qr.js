c54b0ad4b1f1ca04cd3ce5fb2846cbef1f7ea7ec
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-03-16 13:22:53 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,136 +1,16 @@
-/*const fs = require("fs-extra");
 const express = require("express");
-let router = express.Router();
-const app = express();
-const pino = require("pino");
-const { toBuffer } = require("qrcode");
-const { Boom } = require("@hapi/boom");
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    delay,
-    makeCacheableSignalKeyStore,
-    DisconnectReason,
-    makeInMemoryStore
-} = require("@whiskeysockets/baileys");
-
-
-const PORT = process.env.PORT || 5000;
-
-const authInfoPath = __dirname + '/session';
-
-// Vérifier si le répertoire existe déjà
-if (!fs.existsSync(authInfoPath)) {
-  try {
-    // Créer le répertoire
-    fs.mkdirSync(authInfoPath);
-    console.log('Répertoire auth_info_baileys créé avec succès.');
-  } catch (error) {
-    console.error('Erreur lors de la création du répertoire auth_info_baileys :', error);
-  }
-} else {
-  console.log('Le répertoire auth_info_baileys existe déjà.');
-}
-
-// Utilisez fs.emptyDirSync après avoir vérifié ou créé le répertoire
-try {
-  fs.emptyDirSync(authInfoPath);
-  console.log('Contenu du répertoire auth_info_baileys vidé avec succès.');
-} catch (error) {
-  console.error('Erreur lors du vidage du répertoire auth_info_baileys :', error);
-}
-
-
-app.use('/qr-code', async (req, res) => {
- // const { default: OvlWASocket, useMultiFileAuthState, Browsers, delay, DisconnectReason, makeInMemoryStore } = require("@sampandey001/baileys");
-const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
-
-  async function ovls() {
-    const { state, saveCreds } = await useMultiFileAuthStp2ĺĺate(authInfoPath);
-    try {
-      let ovl = makeWASocket({ 
-        printQRInTerminal: false,
-        logger: pino({ level: "silent" }), 
-        browser: [ "Ubuntu", "Chrome", "20.0.04" ],
-        auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "silent"}).child({level: "silent"})),
-                },
-      });
-
-      ovl.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect, qr } = s;
-          if (qr) { res.end(await toBuffer(qr));
-                    res.send(await toBuffer(qr).toString('base64'));
-                  }
-        
-        if (connection == "open"){
-          await delay(3000);
-          let user = ovl.user.id;
-
-          let CREDS = fs.readFileSync('./session/creds.json')
-          var Scan_Id = Buffer.from(CREDS).toString('base64');
-          let ov = ovl.sendMeddage(ovl.user.id, { document: CREDS, mimetype: "Application/json", fileName: "creds.json"}, { quoted: `merci d'avoir choisi ovl-Md`});
-         // let msgsss = await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
-          //await ovl.sendMessage(user, {image: {url: "https://telegra.ph/file/0d81626ca4a81fe93303a.jpg"}, caption: "Merci d'avoir choisie ovl-Md"});
-          await delay(1000);
-          try {
-              await fs.emptyDirSync(authInfoPath);
-          } catch (e) {}
-        }
-
-        ovl.ev.on('creds.update', saveCreds);
-
-        if (connection === "close") {            
-          let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
-          if (reason === DisconnectReason.connectionClosed) {
-            console.log("Connection fermée");
-          } else if (reason === DisconnectReason.connectionLost) {
-            console.log("Connection perdue depuis le serveur !");
-          } else if (reason === DisconnectReason.restartRequired) {
-            console.log("Redémarrage requis, redémarrage en cours...");
-            ovls().catch(err => console.log(err));
-          } else if (reason === DisconnectReason.timedOut) {
-            console.log("Connexion expirée !");
-          }  else {
-            console.log('Connexion fermée avec le bot. Veuillez exécuter à nouveau.');
-            console.log(reason);
-          }
-        }
-      });
-    } catch (err) {
-      console.log(err);
-      await fs.emptyDirSync(authInfoPath);
-    }
-  }
-
-  ovls().catch(async (err) => {
-    console.log(err);
-    await fs.emptyDirSync(authInfoPath);
-    // MADE WITH
-  });
-});
-module.exports =  router*/
-
-
-
-
+const router = express.Router();
 const fs = require("fs-extra");
-const express = require("express");
-const app = express();
-const pino = require("pino");
-const { toBuffer } = require("qrcode");
-const { Boom } = require("@hapi/boom");
 const { makeWASocket, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, DisconnectReason, makeInMemoryStore } = require("@whiskeysockets/baileys");
+const { toBuffer } = require("qrcode");
 
-const PORT = process.env.PORT || 5000;
 const authInfoPath = __dirname + '/session';
 
 // Création ou vidage du répertoire auth_info_baileys
 if (!fs.existsSync(authInfoPath)) {
   try {
     fs.mkdirSync(authInfoPath);
-    console.log('Répertoire  créé avec succès.');
+    console.log('Répertoire créé avec succès.');
   } catch (error) {
     console.error('Erreur lors de la création du répertoire auth_info_baileys :', error);
   }
@@ -145,7 +25,7 @@ try {
   console.error('Erreur lors du vidage du répertoire auth_info_baileys :', error);
 }
 
-app.use('/qr-code', async (req, res) => {
+router.get("/recupererQRCode", async (req, res) => {
   const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
 
   async function ovls() {
@@ -164,16 +44,15 @@ app.use('/qr-code', async (req, res) => {
       ovl.ev.on("connection.update", async (s) => {
         const { connection, lastDisconnect, qr } = s;
         if (qr) { 
-          res.end(await toBuffer(qr));
-         // res.send(await toBuffer(qr).toString('base64'));
+          let qrData = await toBuffer(qr);
+          res.send(qrData.toString('base64'));
         }
         
         if (connection == "open"){
           await delay(3000);
           let user = ovl.user.id;
 
-          let CREDS = fs.readFileSync('./session/creds.json')
-         // var Scan_Id = Buffer.from(CREDS).toString('base64');
+          let CREDS = fs.readFileSync('./session/creds.json');
           let ov = ovl.sendMessage(ovl.user.id, { document: CREDS, mimetype: "Application/json", fileName: "creds.json"}, { quoted: `merci d'avoir choisi ovl-Md`});
 
           await delay(1000);
@@ -213,5 +92,4 @@ app.use('/qr-code', async (req, res) => {
   });
 });
 
-module.exports = app;
-
+module.exports = router;
