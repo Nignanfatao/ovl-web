3c06c771af54b291a14334e88813236484689ab0
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-05-05 12:12:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const express = require('express');
-const { toBuffer } = require('qrcode');
 const app = express();
 __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
-let ovl = require('./qr');
+let data = require('./qr');
 require('events').EventEmitter.defaultMaxListeners = 500;
-const fs = require("fs-extra");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/recupererQRCode", async (req, res) => {
-    ovl.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect, qr } = s;
-        if (qr) { 
-            try {
-                let qrData = await toBuffer(qr);
-                const qrCodeData = qrData.toString('base64');
-                res.send(qrCodeData);
-            } catch (error) {
-                console.error("Erreur lors de la génération du code QR :", error);
-                res.status(500).send("Erreur Interne du Serveur");
-            }
-        }
-    });
-});
+app.use('/recupererQRCode', data);
 
 app.use('/code', code);
 
