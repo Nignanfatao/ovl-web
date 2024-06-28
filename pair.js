1d7ca64af44793b423180307d2bfaa73b7853f0b
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-06-28 08:56:08 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,108 +1,92 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
-const pino = require('pino');
+let router = express.Router()
+const pino = require("pino");
 const {
     default: makeWASocket,
     useMultiFileAuthState,
     delay,
-    makeCacheableSignalKeyStore,
-    DisconnectReason
-} = require('@whiskeysockets/baileys');
+    makeCacheableSignalKeyStore
+} = require("@whiskeysockets/baileys");
 
-const router = express.Router();
-
-function removeFile(filePath) {
-    if (!fs.existsSync(filePath)) return false;
-    fs.rmSync(filePath, { recursive: true, force: true });
-}
-
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
 router.get('/', async (req, res) => {
     let num = req.query.number;
-
-    async function ovlPair() {
-        const { state, saveCreds } = await useMultiFileAuthState('./sessionpair');
-        try {
-            let ovl = makeWASocket({
+        async function XeonPair() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState(`./session`)
+     try {
+            let XeonBotInc = makeWASocket({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'silent' }).child({ level: 'silent' })),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
-                logger: pino({ level: 'silent' }).child({ level: 'silent' }),
-                browser: ['Ubuntu', 'Chrome', '20.0.04'],
-            });
-
-            if (!ovl.authState.creds.registered) {
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+             });
+             if(!XeonBotInc.authState.creds.registered) {
                 await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await ovl.requestPairingCode(num);
-                if (!res.headersSent) {
-                    res.send({ code });
-                }
-            }
-
-            ovl.ev.on('creds.update', saveCreds);
-            ovl.ev.on('connection.update', async (s) => {
-                const { connection, lastDisconnect } = s;
-                if (connection === 'open') {
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await XeonBotInc.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            XeonBotInc.ev.on('creds.update', saveCreds)
+            XeonBotInc.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(10000);
+                    const sessionXeon = fs.readFileSync('./session/creds.json');
+                    const audioxeon = fs.readFileSync('./OneDance.mp3');
+                    XeonBotInc.groupAcceptInvite("Kjm8rnDFcpb04gQNSTbW2d");
+				const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
+				XeonBotInc.sendMessage(XeonBotInc.user.id, {
+                    audio: audioxeon,
+                    mimetype: 'audio/mp4',
+                    ptt: true
+                }, {
+                    quoted: xeonses
+                });
+				await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `*_🛑Do not share this file with anybody_*\n\n© *_Subscribe_* www.youtube.com/@s4salmanyt *_on Youtube_*` }, {quoted: xeonses});
+        await delay(100);
+        return await removeFile('./session');
+        process.exit(0)
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    console.log('Connected to WhatsApp');
-                    let user = ovl.user.id;
-                    let creds = fs.readFileSync('./sessionpair');
-                    await ovl.groupAcceptInvite('LhnBI1Igg7W1ZgyqT8gIxa');
-                    const scanId = Buffer.from(creds).toString('base64');
-                    await ovl.sendMessage(user, { text: `Ovl;;; ${scanId}` });
-                    await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/4d918694f786d7acfa3bd.jpg' }, caption: "Merci d'avoir choisi OVL-MD" });
-                    await delay(1000);
-                    removeFile('./sessionpair');
-                    process.exit(0);
-                } else if (connection === 'close') {
-                    const shouldReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
-                    console.log('Connection closed. Reconnecting...', shouldReconnect);
-                    if (shouldReconnect) {
-                        await delay(10000);
-                        ovlPair();
-                    } else {
-                        console.log('Logged out from WhatsApp.');
-                    }
+                    XeonPair();
                 }
             });
-
-            ovl.ev.on('messages.upsert', (message) => {
-                console.log('Received a message', message);
-            });
-
-            ovl.ev.on('connection.update', (update) => {
-                console.log('Connection update', update);
-            });
-
-            ovl.ev.on('contacts.update', (update) => {
-                console.log('Contacts update', update);
-            });
-
         } catch (err) {
-            console.log('Error occurred:', err);
-            removeFile('./sessionpair');
-            if (!res.headersSent) {
-                res.send({ code: 'Application indisponible' });
-            }
+            console.log("service restated");
+            await removeFile('./session');
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
         }
     }
-
-    return ovlPair();
+    return await XeonPair()
 });
 
 process.on('uncaughtException', function (err) {
-    let e = String(err);
-    if (e.includes('conflict')) return;
-    if (e.includes('Socket connection timeout')) return;
-    if (e.includes('not-authorized')) return;
-    if (e.includes('rate-overlimit')) return;
-    if (e.includes('Connection Closed')) return;
-    if (e.includes('Timed Out')) return;
-    if (e.includes('Value not found')) return;
-    console.log('Caught exception: ', err);
-});
+let e = String(err)
+if (e.includes("conflict")) return
+if (e.includes("Socket connection timeout")) return
+if (e.includes("not-authorized")) return
+if (e.includes("rate-overlimit")) return
+if (e.includes("Connection Closed")) return
+if (e.includes("Timed Out")) return
+if (e.includes("Value not found")) return
+console.log('Caught exception: ', err)
+})
 
 module.exports = router
