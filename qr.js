dda0d454540729d64b5d5fe553834f66f0a0c70c
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-07-29 01:26:55 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,25 +1,14 @@
 const express = require('express');
+const router = express.Router();
 const fs = require('fs-extra');
-const path = require('path');
 const pino = require('pino');
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    delay,
-    makeCacheableSignalKeyStore
-} = require("@whiskeysockets/baileys");
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, Browsers } = require('@whiskeysockets/baileys');
 const { Boom } = require('@hapi/boom');
 const { toDataURL } = require('qrcode');
-
-const router = express.Router();
+const { delay } = require('@whiskeysockets/baileys');
 
 // Chemin pour stocker les informations d'authentification
 const authInfoPath = './auth';
-
-function removeFile(filePath) {
-    if (!fs.existsSync(filePath)) return false;
-    fs.rmSync(filePath, { recursive: true, force: true });
-}
 
 router.get('/', async (req, res) => {
     try {
@@ -31,21 +20,16 @@ router.get('/', async (req, res) => {
             height: req.query.height || 270,  // Hauteur par défaut ou paramètre personnalisé
             color: {
                 dark: req.query.darkColor || '#000000',  // Couleur sombre (par défaut noir)
-                light: req.query.lightColor || '#ffffff' // Couleur claire (par défaut blanc)
+                light: req.query.lightColor || 'rgba(255,255,255,0.5)' // Couleur claire (par défaut blanc)
             }
         };
 
         let ovl = makeWASocket({
-            auth: {
-                creds: state.creds,
-                keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "silent" }).child({ level: "silent" })),
-            },
             printQRInTerminal: false,
-            logger: pino({ level: "silent" }).child({ level: "silent" }),
-            browser: ["Ubuntu", "Chrome", "20.0.04"],
+            logger: pino({ level: 'silent' }),
+            browser: Browsers.baileys('Desktop'),
+            auth: state
         });
-
-        let sent = false; // Variable pour suivre si une réponse a déjà été envoyée
 
         ovl.ev.on('connection.update', async (s) => {
             const { connection, lastDisconnect, qr } = s;
@@ -55,26 +39,23 @@ router.get('/', async (req, res) => {
                     const qrDataURL = await toDataURL(qr, qrOptions);
                     const data = qrDataURL.split(',')[1]; // Envoyer seulement la partie base64 de l'URL
                     res.send(data);
-                    sent = true; // Marquer que la réponse a été envoyée
                 } catch (err) {
                     console.error('Erreur lors de la génération du QR code personnalisé :', err);
                     res.status(500).send('Erreur lors de la génération du QR code personnalisé');
-                    sent = true; // Marquer que la réponse a été envoyée
-                }
+                    }
             }
 
-            if (connection === 'open' && !sent) {
+            if (connection === 'open') {
                 await delay(3000);
                 let user = ovl.user.id;
 
-                let CREDS = fs.readFileSync(authInfoPath);
+                let CREDS = fs.readFileSync(authInfoPath + '/creds.json');
                 var Scan_Id = Buffer.from(CREDS).toString('base64');
-                await ovl.groupAcceptInvite("LhnBI1Igg7W1ZgyqT8gIxa");
                 await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
-                await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/4d918694f786d7acfa3bd.jpg' }, caption: "Merci d'avoir choisi OVL-MD" });
+                await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/0d81626ca4a81fe93303a.jpg' }, caption: "Merci d'avoir choisi OVL-MD" });
                 await delay(1000);
                 try {
-                    fs.emptyDirSync(authInfoPath);
+                    await fs.emptyDirSync(authInfoPath);
                 } catch (e) {
                     console.log('Erreur lors de la suppression des fichiers :', e);
                 }
@@ -82,7 +63,7 @@ router.get('/', async (req, res) => {
 
             ovl.ev.on('creds.update', saveCreds);
 
-            if (connection === 'close' && !sent) {
+            if (connection === 'close') {
                 let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
                 if (reason === DisconnectReason.connectionClosed) {
                     console.log('Connection fermée');
@@ -98,8 +79,7 @@ router.get('/', async (req, res) => {
                     console.log(reason);
                 }
                 res.status(500).send('Connexion fermée avec le bot. Veuillez exécuter à nouveau.');
-                sent = true; // Marquer que la réponse a été envoyée
-            }
+                }
         });
 
         // Gestion des erreurs
@@ -107,26 +87,13 @@ router.get('/', async (req, res) => {
             console.error('Erreur de connexion :', err);
             if (!sent) {
                 res.status(500).send('Erreur lors de la génération du QR code');
-                sent = true; // Marquer que la réponse a été envoyée
-            }
+               }
         });
     } catch (err) {
         console.error('Erreur lors de l\'authentification :', err);
-        fs.emptyDirSync(authInfoPath);
+        await fs.emptyDirSync(authInfoPath);
         res.status(500).send('Erreur lors de la génération du QR code');
     }
-});
-
-process.on('uncaughtException', function (err) {
-    let e = String(err);
-    if (e.includes("conflict")) return;
-    if (e.includes("Socket connection timeout")) return;
-    if (e.includes("not-authorized")) return;
-    if (e.includes("rate-overlimit")) return;
-    if (e.includes("Connection Closed")) return;
-    if (e.includes("Timed Out")) return;
-    if (e.includes("Value not found")) return;
-    console.log('Caught exception: ', err);
 });
 
 module.exports = router;
