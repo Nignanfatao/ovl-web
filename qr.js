07f4afdddc221c4110bfb85536f75aae8d8d944e
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-10-30 19:13:27 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
-let router = express.Router()
-const pino = require("pino");
+const os = require('os');
 const path = require('path');
+const pino = require("pino");
+let router = express.Router();
 const {
     default: makeWASocket,
     useMultiFileAuthState,
@@ -11,96 +12,100 @@ const {
 } = require("@whiskeysockets/baileys");
 const { toDataURL } = require('qrcode');
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+};
+
 router.get('/', async (req, res) => {
     async function ovlQr() {
+        // Utiliser un répertoire temporaire
+        const authDir = path.join(os.tmpdir(), 'auth');
+        if (!fs.existsSync(authDir)) {
+            fs.mkdirSync(authDir, { recursive: true });
+        }
+
         const {
             state,
             saveCreds
-  } = await useMultiFileAuthState('./auth');
-     try {
+        } = await useMultiFileAuthState(authDir);
+
+        try {
             let ovl = makeWASocket({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
                 },
                 printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: [ "Ubuntu", "Chrome", "20.0.04" ],
-             });
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: ["Ubuntu", "Chrome", "20.0.04"],
+            });
 
-         const qrOptions = {
-            width: req.query.width || 270,   // Largeur par défaut ou paramètre personnalisé
-            height: req.query.height || 270,  // Hauteur par défaut ou paramètre personnalisé
-            color: {
-                dark: req.query.darkColor || '#000000',  // Couleur sombre (par défaut noir)
-                light: req.query.lightColor || '#ffff' // Couleur claire (par défaut blanc)
-            }
-        };
-         
-             ovl.ev.on('connection.update', async (s) => {
-            const { connection, lastDisconnect, qr } = s;
-            if (qr) {
-                try {
-                    // Générer le QR code avec les options personnalisées
-                    const qrDataURL = await toDataURL(qr, qrOptions);
-                    const data = qrDataURL.split(',')[1]; // Envoyer seulement la partie base64 de l'URL
-                    res.send(data);
-                } catch (err) {
-                    console.error('Erreur lors de la génération du QR code personnalisé :', err);
-                    res.status(500).send('Erreur lors de la génération du QR code personnalisé');
+            const qrOptions = {
+                width: req.query.width || 225,
+                height: req.query.height || 225,
+                color: {
+                    dark: req.query.darkColor || '#000000',
+                    light: req.query.lightColor || '#ffffff'
+                }
+            };
+
+            ovl.ev.on('connection.update', async (s) => {
+                const { connection, lastDisconnect, qr } = s;
+                if (qr) {
+                    try {
+                        const qrDataURL = await toDataURL(qr, qrOptions);
+                        const data = qrDataURL.split(',')[1];
+                        res.send(data);
+                    } catch (err) {
+                        console.error('Erreur lors de la génération du QR code personnalisé :', err);
+                        res.status(500).send('Erreur lors de la génération du QR code personnalisé');
                     }
-            }
-             });
-                                                  
-            ovl.ev.on('creds.update', saveCreds)
-                 
-            ovl.ev.on("connection.update", async (s) => {
-                const {
-                    connection,
-                    lastDisconnect
-                } = s;
-                if (connection == "open") {
-                await delay(1000);
-                let user = ovl.user.id;
+                }
+            });
 
-                let CREDS = fs.readFileSync('./auth/creds.json');
-                var Scan_Id = Buffer.from(CREDS).toString('base64');
-               await ovl.groupAcceptInvite("LhnBI1Igg7W1ZgyqT8gIxa");     
-                await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
-                await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/4d918694f786d7acfa3bd.jpg' }, caption: "Merci d'avoir choisi OVL-MD" });
-                await delay(1000);
-    return await removeFile('./auth');
-        process.exit(0)
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+            ovl.ev.on('creds.update', saveCreds);
+
+            ovl.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection == "open") {
+                    await delay(1000);
+                    let user = ovl.user.id;
+
+                    let CREDS = fs.readFileSync(path.join(authDir, 'creds.json'));
+                    var Scan_Id = Buffer.from(CREDS).toString('base64');
+                    await ovl.groupAcceptInvite("KMvPxy6Xw7yA49xRLNCxEb");
+                    await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
+                    await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/4d918694f786d7acfa3bd.jpg' }, caption: "Merci d'avoir choisi OVL-MD" });
+                    await delay(1000);
+                    removeFile(authDir);
+                    process.exit(0);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     ovlQr();
                 }
             });
         } catch (err) {
             console.log("service restated");
-            await removeFile('./auth');
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
-     }
+            removeFile(authDir);
+            if (!res.headersSent) {
+                res.send({ code: "Service Unavailable" });
+            }
+        }
     }
-    return await ovlQr()
+    return await ovlQr();
 });
 
 process.on('uncaughtException', function (err) {
-let e = String(err)
-if (e.includes("conflict")) return
-if (e.includes("Socket connection timeout")) return
-if (e.includes("not-authorized")) return
-if (e.includes("rate-overlimit")) return
-if (e.includes("Connection Closed")) return
-if (e.includes("Timed Out")) return
-if (e.includes("Value not found")) return
-console.log('Caught exception: ', err)
-})
+    let e = String(err);
+    if (e.includes("conflict")) return;
+    if (e.includes("Socket connection timeout")) return;
+    if (e.includes("not-authorized")) return;
+    if (e.includes("rate-overlimit")) return;
+    if (e.includes("Connection Closed")) return;
+    if (e.includes("Timed Out")) return;
+    if (e.includes("Value not found")) return;
+    console.log('Caught exception: ', err);
+});
 
-module.exports = router
+module.exports = router;
