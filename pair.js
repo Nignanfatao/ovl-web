d7303cedecf61f3a26af0e45141bdb98b1bc0966
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2024-10-31 13:08:07 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -3,16 +3,12 @@ const axios = require('axios');
 const fs = require('fs');
 let router = express.Router();
 const pino = require("pino");
-const path = require('path');
 const {
     default: makeWASocket,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore
 } = require("@whiskeysockets/baileys");
-
-// Clé API de Pastebin
-//const pastebinApiKey = 'E4AVswX1Fj6CRitqofpUwTX4Y2VdDmMR';
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
@@ -36,11 +32,11 @@ router.get('/', async (req, res) => {
                 browser: [ "Ubuntu", "Chrome", "20.0.04" ],
             });
 
-            if(!ovl.authState.creds.registered) {
+            if (!ovl.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g,'');
                 const code = await ovl.requestPairingCode(num);
-                if(!res.headersSent) {
+                if (!res.headersSent) {
                     await res.send({ code });
                 }
             }
@@ -52,31 +48,26 @@ router.get('/', async (req, res) => {
                 if (connection == "open") {
                     await delay(1000);
                     let user = ovl.user.id;
-                    let CREDS = fs.readFileSync('./auth/creds.json');
-                    var Scan_Id = Buffer.from(CREDS).toString('base64');
-                    let data = await fs.readFileSync('./auth/creds.json','utf-8');
-          const response = await axios.post('http://paste.c-net.org/',`${btoa(data)}`, {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
-         // let c = output.data.split('/')[3]
-          
-                    // Envoi de la session à Pastebin
-                  /*  const response = await axios.post('https://pastebin.com/api/api_post.php', new URLSearchParams({
-                            api_dev_key: pastebinApiKey,
-                            api_option: 'paste',
-                            api_paste_code: CREDS,
-                            api_paste_expire_date: 'N'
-                        }).toString(), {
-                            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-                        });*/
+                    let CREDS = fs.readFileSync('./auth/creds.json', 'utf-8');
 
-                        const pastebinLink = response.data.split('/')[3];
-                        console.log(`Lien de Pastebin : ${response.data}`);
-                    await ovl.groupAcceptInvite("KMvPxy6Xw7yA49xRLNCxEb");
-                    await ovl.sendMessage(user, { text: `Ovl-MD_${pastebinLink}_SESSION-ID` });
-                    await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/4d918694f786d7acfa3bd.jpg' }, caption: "Merci d'avoir choisi OVL-MD voici votre SESSION-ID⏏️" });
-                    
-                    await delay(1000);  
-                    await removeFile('./sessionpair');
-                    process.exit(0);
+                    // Envoi de la session à paste.c-net.org
+                    try {
+                        const response = await axios.post('http://paste.c-net.org/', CREDS, {
+                            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                        });
+
+                        const pastebinLink = response.data;
+                        console.log(`Lien de Pastebin : ${pastebinLink}`);
+                        await ovl.groupAcceptInvite("KMvPxy6Xw7yA49xRLNCxEb");
+                        await ovl.sendMessage(user, { text: `Ovl-MD_${pastebinLink}_SESSION-ID` });
+                        await ovl.sendMessage(user, { image: { url: 'https://telegra.ph/file/4d918694f786d7acfa3bd.jpg' }, caption: "Merci d'avoir choisi OVL-MD voici votre SESSION-ID⏏️" });
+                        
+                        await delay(1000);  
+                        await removeFile('./sessionpair');
+                        process.exit(0);
+                    } catch (error) {
+                        console.error("Erreur lors de l'envoi vers paste.c-net.org :", error);
+                    }
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     ovlPair();
@@ -85,7 +76,7 @@ router.get('/', async (req, res) => {
         } catch (err) {
             console.log("Service redémarré");
             await removeFile('./sessionpair');
-            if(!res.headersSent) {
+            if (!res.headersSent) {
                 await res.send({ code: "Service Unavailable" });
             }
         }
