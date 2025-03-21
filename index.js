3098d777e9e64e844125fe3026dc3dc5c83d0031
Ainz <140420451+Nignanfatao@users.noreply.github.com>
2025-03-21 18:57:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { exec } = require('child_process');
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
 const router = require('./qr');
@@ -26,5 +27,20 @@ app.use('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
+
+setInterval(() => {
+    console.log("🔄 Exécution de 'pm2 restart all'...");
+    exec('pm2 restart all', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`❌ Erreur : ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`⚠️ stderr: ${stderr}`);
+            return;
+        }
+        console.log(`✅ stdout: ${stdout}`);
+    });
+}, 1800000);
 
 module.exports = app;
